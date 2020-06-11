e7c84838b60341700928dae2dd648743103e590d
Portisch <hugo.portisch@yahoo.de>
2020-06-11 10:00:13 +0200

    dtb-xml: add 'dangerous' pin request

--- packages/sysutils/dtb-xml/package.mk
+++ packages/sysutils/dtb-xml/package.mk
@@ -16,3 +16,28 @@ makeinstall_target() {
     install -m 0755 ${PKG_DIR}/scripts/dtb-xml.sh ${INSTALL}/usr/lib/coreelec/dtb-xml
     install -m 0644 ${PKG_DIR}/config/dtb.xml ${INSTALL}/usr/share/bootloader/dtb.xml
 }
+
+# ---------------------------------------
+# No /flash/dtb.xml exist:
+# On first access of a variable by 'get_dtbxml_value' or 'get_dtbxml_multivalues'
+# by CE settings current defined default values are read by dtb-xml.sh and
+# migrated to /flash/dtb.xml.
+#
+# If a matching value is found the option is set in dtb.xml. If no matching
+# value is found or the path is not found in dtb.img it get set to 'migrated'.
+# On next access of a option set to 'migrated' the dtb-xml.sh script try again
+# to read current value from dtb.img as it maybe turn to applicable after a
+# update of dtb.img by tar update.
+# -> need to check if a valid option is set in dtb.xml but it become non applicable
+#
+# /flash/dtb.xml get only be edited by dtb-xml.sh script and remain untouched
+# on update of the system. When a update is done user defined values are
+# applied to /flash/dtb.img by dtb.xml after update of dtb.img itself so no
+# extra reboot is needed.
+#
+# -> how can a new default value be forced by update?
+# Now if a new default value get introduced it will be overwritten by the users
+# dtb.xml value after update of dtb.img.
+# -> somehow the value need to be removed or updated in dtb.xml
+#
+# On bricked devices: remove /flash/dtb.xml and copy default dtb.img on /flash
