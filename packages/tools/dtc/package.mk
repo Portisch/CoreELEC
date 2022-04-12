51d3ae98b81afb26de111b0d4930e8d77bb62524
Portisch <hugo.portisch@yahoo.de>
2022-04-12 13:54:23 +0200

    fixup dtc

--- packages/tools/dtc/package.mk
+++ packages/tools/dtc/package.mk
@@ -17,12 +17,11 @@ PKG_MAKE_OPTS_TARGET="dtc fdtput fdtget fdtdump libfdt"
 PKG_MAKE_OPTS_HOST="dtc libfdt"
 
 pre_configure() {
-  export LDLIBS_dtc="-lz"
   export EXTRA_CFLAGS="-I${TOOLCHAIN}/include"
 }
 
-pre_configure_target() {
-  export LDFLAGS="-L$(get_install_dir zlib)/usr/lib"
+pre_configure_host() {
+  export LDLIBS_dtc="-lz"
 }
 
 pre_make_host() {
